cfbc9faf337c9e7581e7d102019e233a85ba951c
Ahmed Hassan <ahmed.hassan398@outlook.com>
2025-06-26 04:52:05 +0500

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,70 @@
+﻿using _WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace _WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly PracticeWebAppContext context;
+
+        public StudentsController(PracticeWebAppContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await this.context.Students.ToListAsync();
+            if (data != null)
+                return Ok(data);
+            else
+                return NoContent();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudents(int id)
+        {
+            var data = await this.context.Students.FindAsync(id);
+            if (data != null)
+                return Ok(data);
+            else
+                return NotFound();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student s)
+        {
+            await this.context.Students.AddAsync(s);
+            await this.context.SaveChangesAsync();
+            return Ok(s);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int? id, Student s)
+        {
+            if (id != s.Id)
+            {
+                return BadRequest();
+            }
+            this.context.Entry(s).State = EntityState.Modified;
+            await this.context.SaveChangesAsync();
+            return Ok(s);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int? id)
+        {
+            var data = await this.context.Students.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            this.context.Students.Remove(data);
+            await this.context.SaveChangesAsync();
+            return Ok(data);
+        }
+
+    }
+}
---
+++ Models/PracticeWebAppContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace _WebApi.Models;
+
+public partial class PracticeWebAppContext : DbContext
+{
+    public PracticeWebAppContext()
+    {
+    }
+
+    public PracticeWebAppContext(DbContextOptions<PracticeWebAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__students__3214EC0763CB8956");
+
+            entity.ToTable("students");
+
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
